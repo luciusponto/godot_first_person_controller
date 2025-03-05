c499dbc675a13f7747705442fe36e8d0f921815a
Luciano Scilletta <luciusponto@users.noreply.github.com>
2025-03-05 14:43:57 +0000

    Make task id property readonly in inspector

--- addons/scene_task_tracker/scripts/task.gd
+++ addons/scene_task_tracker/scripts/task.gd
@@ -1,59 +1,57 @@
 class_name SttTaskItem
 extends Resource
 
-#signal task_changed
-#
-#enum TaskTypes {
-#	BUG,
-#	FEATURE,
-#	TECHNICAL_IMPROVEMENT,
-#	POLISH,
-#	REGRESSION_TEST,
-#	UNKNOWN,
-#}
+signal task_changed
 
-@export var id : int = -1
+enum TaskTypes {
+	BUG,
+	FEATURE,
+	TECHNICAL_IMPROVEMENT,
+	POLISH,
+	REGRESSION_TEST,
+	UNKNOWN,
+}
 
-#@export_multiline var description: String = "Task description here":
-#	get:
-#		return description
-#	set(text):
-#		description = text
-#		task_changed.emit()
-#
-#@export_multiline var details: String:
-#	get:
-#		return details
-#	set(text):
-#		details = text
-#		task_changed.emit()
-#
-#
-#@export var task_type: TaskTypes = TaskTypes.UNKNOWN:
-#	get:
-#		return task_type
-#	set(value):
-#		task_type = value
-#		task_changed.emit()
-#
-#
-#@export_range(1, 5) var priority: int = 1:
-#	get:
-#		return priority
-#	set(value):
-#		priority = value
-#		task_changed.emit()
-#
-#@export var fixed: bool = false:
-#	get:
-#		return fixed
-#	set(value):
-#		fixed = value
-#		task_changed.emit()
+@export_multiline var description: String = "Task description here":
+	get:
+		return description
+	set(text):
+		description = text
+		task_changed.emit()
+
+@export_multiline var details: String:
+	get:
+		return details
+	set(text):
+		details = text
+		task_changed.emit()
+
+
+@export var task_type: TaskTypes = TaskTypes.UNKNOWN:
+	get:
+		return task_type
+	set(value):
+		task_type = value
+		task_changed.emit()
+
+
+@export_range(1, 5) var priority: int = 1:
+	get:
+		return priority
+	set(value):
+		priority = value
+		task_changed.emit()
+
+@export var fixed: bool = false:
+	get:
+		return fixed
+	set(value):
+		fixed = value
+		task_changed.emit()
+		
+@export_group("Debug")
+@export var id : int = -1		
 		
 func _validate_property(property):
-	print("Validating " + property.name)
 	if property.name == "id":
-		property.usage = PROPERTY_USAGE_READ_ONLY
-		
-
+		property.usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_READ_ONLY | PROPERTY_USAGE_INTERNAL
--- addons/scene_task_tracker/scripts/task_database.gd
+++ addons/scene_task_tracker/scripts/task_database.gd
@@ -1,11 +1,32 @@
+@tool
 class_name SttTaskDatabase
 extends Resource
 
+#const Task = preload("task.gd")
+
+@export var add_new_task := false:
+	set(value):
+		value = false
+		var task = SttTaskItem.new()
+		add_task(task)
+
 @export var tasks : Array[SttTaskItem] = []
 
-@export var last_id : int = -1
+@export_group("Debug")
+@export_storage var last_id : int = -1
+@export_storage var previous_tasks_len = 0
 
-#func _validate_property(property):
-#	print("validating properties of SttTaskDatabase")
-#	if property.name == "last_id":
-#		property.usage |= PROPERTY_USAGE_
+func _get_new_task_id() -> int:
+	last_id += 1
+	return last_id
+
+func add_task(task : SttTaskItem):
+	task.id = _get_new_task_id()
+	print("adding new task with id " + str(last_id))
+	tasks.append(task)
+	notify_property_list_changed()
+	
+func _validate_property(property):
+	print("Validating property " + property.name)
+	if property.name == "last_id":
+		property.usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_READ_ONLY | PROPERTY_USAGE_INTERNAL
